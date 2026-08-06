7907ac496e72af30381165c8d99fe7534d0ddfe5
plaserd <cmkin402@gmail.com>
2026-08-06 11:13:07 -0400

    button functionality

--- messagehandler.js
+++ messagehandler.js
@@ -1,5 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const templates1 = document.getElementById("templates1");
+const templateButtons = document.querySelectorAll(".templatesButton");
+const wordsButtons = document.querySelectorAll(".wordsButton");
+const conjunctionButton = document.getElementById("conjunctionsButton");
+const gesturesButton = document.getElementById("gesturesButton");
 
-    // Fetch file that will replace dropdown optins
+templateButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log("Templates button");
+    })
+});
+
+wordsButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log("Words Button");
+    })
+}) 
+
+conjunctionButton.addEventListener('click', () => {
+    console.log("Conjunctions Button");
+})
+
+gesturesButton.addEventListener('click', () => {
+    console.log("Gestures Button");
 })
