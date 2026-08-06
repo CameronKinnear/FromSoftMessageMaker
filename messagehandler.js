ea34bc8f45d245a2cc664275adc211f98018dfc4
plaserd <cmkin402@gmail.com>
2026-08-06 12:04:29 -0400

    lines are filled in with buttons instead of raw text

--- messagehandler.js
+++ messagehandler.js
@@ -34,7 +34,59 @@ gesturesButton.addEventListener('click', () => {
 const fillInText = document.getElementById("fillInText")
 
 async function GetFillInText(textType) {
+
+    RemoveButtons();
+    RemoveButtonsCol2();
+
     const response = await fetch(textType + ".txt")
     const rawData = await response.text();
-    fillInText.innerHTML = rawData;
+    const data = await rawData.split("\n");
+
+    const fillTextArea = document.getElementById("fillInText");
+    for (const text of data) {
+        const newButton = document.createElement('button');
+        newButton.innerHTML = text;
+        newButton.style.width = "auto"
+        if (textType == "Words") {
+            newButton.addEventListener('click', () => {
+                GetFillInWords(text);
+            });
+        }
+        fillTextArea.appendChild(newButton);
+    }
+}
+
+
+async function GetFillInWords(wordsType) {
+
+    RemoveButtonsCol2();
+    
+    const response = await fetch("Words/" + wordsType + ".txt")
+    const rawData = await response.text();
+    const data = await rawData.split("\n");
+
+    const fillTextArea = document.getElementById("fillInTextCol2");
+    for (const text of data) {
+        const newButton = document.createElement('button');
+        newButton.innerHTML = text;
+        newButton.style.width = "auto"
+        fillTextArea.appendChild(newButton);
+    }
+}
+
+
+async function RemoveButtons() {
+    const fillTextArea = document.getElementById("fillInText");
+
+    while (fillTextArea.firstChild) {
+        fillTextArea.removeChild(fillTextArea.firstChild);
+    }
+}
+
+async function RemoveButtonsCol2() {
+    const fillTextAreaCol2 = document.getElementById("fillInTextCol2");
+
+    while (fillTextAreaCol2.firstChild) {
+        fillTextAreaCol2.removeChild(fillTextAreaCol2.firstChild);
+    }
 }
